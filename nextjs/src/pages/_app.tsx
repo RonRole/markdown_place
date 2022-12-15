74a675eb63a586d120c8c83d903bda13bb45048e
RonRole <ronrolenewadress@gmail.com>
2022-12-15 19:14:25 +0900

    laravel Dockerfileの使用イメージバージョン指定(固定した方が想定外の事態になりにくいと思い...)
    Laravel Sanctum + Laravel Fortifyのセットアップ
    公式ドキュメントに沿って設定
    https://readouble.com/laravel/8.x/ja/fortify.html
    https://readouble.com/laravel/8.x/ja/sanctum.html
    
    corsの設定用に、環境変数にフロントのアドレス追加
    
    nextjs
    nodeのバージョンの問題でaxiosにエラーが出たので、
    Dockerfileの使用イメージを指定して回避
    
    出たエラー
    https://github.com/axios/axios/issues/5221
    
    axiosを追加
    axiosのデフォルト設定を_app.tsxに記載(nextjsのエントリポイントとして)
    
    laravelとの認証機能の確認用にテストページ配置

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_APP_API_URL;
+axios.defaults.withCredentials = true;
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ nextjs/src/pages/test/index.tsx
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+export default function Test() {
+    const handleCSRFTest = () => {
+        axios.get('/sanctum/csrf-cookie').then(response=> {
+            console.log(response);
+        });
+    }
+    // 送信するデータの名称はlaravel fortifyに合わせる必要がある
+    const handleSignUp = () => {
+        axios.post('/api/register', {
+            name: 'test user name',
+            email: 'testuser@example.com',
+            password: 'test_password',
+            password_confirmation: 'test_password'
+        }).then(response=>console.log(response));
+    }
+    const handleGetUser = () => {
+        axios.get('/api/user').then(response=>{
+            console.log(response);
+        });
+    }
+    return (
+        <>
+            <h1>laravelとの認証テスト</h1>
+            <ol>
+                <li><button onClick={handleCSRFTest}>laravel側のcsrf保護を有効にする</button></li>
+                <li><button onClick={handleSignUp}>ユーザー登録</button></li>
+                <li><button onClick={handleGetUser}>認証が必要なurlにリクエスト(/api/user)</button></li>
+            </ol>
+        </>
+    )
+}

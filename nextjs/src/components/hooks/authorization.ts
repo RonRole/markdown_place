2a3fda66bbf9bd5ad6c9dff04626738e9b8e2aa1
RonRole <ronrolenewadress@gmail.com>
2022-12-20 16:44:45 +0900

    hooksの関数をコールバックにするのを忘れて画面が無限に切り替わっていたので、修正

--- nextjs/src/components/hooks/authorization.ts
+++ nextjs/src/components/hooks/authorization.ts
@@ -1,4 +1,5 @@
 import axios, { Axios, AxiosResponse } from "axios";
+import React from "react";
 import AuthStatus from "../../domains/entities/auth-status";
 
 export type UseAuthorizationReturnType = {
@@ -7,22 +8,24 @@ export type UseAuthorizationReturnType = {
 }
 
 export default function useAuthorization() : UseAuthorizationReturnType {
-    return {
-        async getStatus(): Promise<AuthStatus>  {
-            const response = await axios.get('/api/user');
-            if(response?.status === 200) {
-                return 'authorized';
-            }
-            return 'unauthorized';
-        },
-        async auth(email: string, password: string): Promise<AuthStatus> {
-            await axios.get('/sanctum/csrf-cookie');
-            const response = await axios.post('/api/login', {
-                email,
-                password
-            });
-            if(response?.status === 200) return 'authorized';
-            return 'unauthorized';
+    const getStatus = React.useCallback(async ()=>{
+        const response = await axios.get('/api/user');
+        if(response?.status === 204) {
+            return 'authorized';
         }
+        return 'unauthorized';
+    }, []);
+    const auth = React.useCallback(async (email:string, password:string)=>{
+        await axios.get('/sanctum/csrf-cookie');
+        const response = await axios.post('/api/login', {
+            email,
+            password
+        });
+        if(response?.status === 200) return 'authorized';
+        return 'unauthorized';
+    }, []);
+    return {
+        getStatus,
+        auth,
     }
 }

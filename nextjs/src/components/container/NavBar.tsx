6156e5ece6e27ccb3b5b09e5ea64f552f19728c5
RonRole <ronrolenewadress@gmail.com>
2022-12-23 14:16:41 +0900

    ナビゲーションバーのデザイン変更 ログアウトボタンを右端に置く
    親コンポーネント(ToolBar)のflex-glowを1にして、
    ログアウトボタン以外のコンポーネント(Container)でflex-glow:1を埋めるようにする

--- nextjs/src/components/container/NavBar.tsx
+++ nextjs/src/components/container/NavBar.tsx
@@ -20,16 +20,16 @@ export function NavBar({children}: NavBarProps) {
     return (
         <>
             <AppBar position='sticky'>
-                <Toolbar>
-                    <Container sx={{display:'flex'}} maxWidth='xl' disableGutters>
+                <Toolbar sx={{flexGrow:1}}>
+                    <Container sx={{display:'flex', flexGrow: 1}} maxWidth='xl' disableGutters>
                         <Typography sx={{mr:2}} variant="h6" component={Link} href='/'>Sawai Kei</Typography>
                         {links.map((linkSrc, i)=>(
                             <Link key={i} href={linkSrc.path} passHref>
                                 <Button sx={{color:'white'}}>{linkSrc.display}</Button>
                             </Link>
                         ))}
-                        <LogoutButton />
                     </Container>
+                    <LogoutButton sx={{whiteSpace:'nowrap'}} />
                 </Toolbar>
             </AppBar>
             {children}

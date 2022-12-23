6c29c0cb2ef1252a62834ca28f1a01a832b545fa
RonRole <ronrolenewadress@gmail.com>
2022-12-23 15:15:49 +0900

    laravelでセッション保持にredisを使用する
    predis/predisパッケージを使用
    とりあえずデフォルトの設定で使用(パスワードなし、schemeはtcp)
    
    本番ではパスワード有りのtlsで接続するようにしたい

--- laravel/src/config/database.php
+++ laravel/src/config/database.php
@@ -119,7 +119,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -127,6 +127,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),

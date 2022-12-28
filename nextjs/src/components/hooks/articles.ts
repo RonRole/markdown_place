422792724a1a69b3362a38a3291d2ed17594d01a
RonRole <ronrolenewadress@gmail.com>
2022-12-28 12:38:39 +0900

    Articleクラスの構造を修正 EditNewArticleFormを書き直し

--- nextjs/src/components/hooks/articles.ts
+++ nextjs/src/components/hooks/articles.ts
@@ -1,5 +1,21 @@
+import axios from 'axios';
+import React from 'react';
 import Article from '../../domains/article';
 
+type CreateArticleProps = Omit<Article, 'id'>;
 export type UseArticleFunctions = {
-    create(article: Article): Promise<boolean>;
+    create(article: CreateArticleProps): Promise<boolean>;
 };
+
+export function useArticles(): UseArticleFunctions {
+    const create = React.useCallback(async (props: CreateArticleProps) => {
+        const result = await axios.post('/api/articles', {
+            title: props.title,
+            content: props.content,
+        });
+        return result.status === 200;
+    }, []);
+    return {
+        create,
+    };
+}

a9f46e7d59ba4619b74be53fd9b04b4f6c5c4862
RonRole <ronrolenewadress@gmail.com>
2023-01-17 16:39:11 +0900

    github actionsの設定を追加
    環境変数はARGでDockerコンテナに入れ込むのでなく、
    それぞれで.envファイルを使用するように変更

--- postgres/scripts/initialize.sql
+++ postgres/scripts/initialize.sql
@@ -1,14 +1,29 @@
 -- データベース作成
-CREATE DATABASE sample_db;
+CREATE DATABASE markdown_place_db;
 
 -- データベース切り替え
-\c sample_db
+\c markdown_place_db
 
 -- スキーマ作成
-CREATE SCHEMA sample_schema;
+CREATE SCHEMA markdown_place_schema;
 
 -- ロール作成
-CREATE ROLE sample_role WITH LOGIN PASSWORD 'sample_role';
+CREATE ROLE markdown_place_role WITH LOGIN PASSWORD 'markdown_place_role';
 
 -- 権限付与
-GRANT ALL PRIVILEGES ON SCHEMA sample_schema TO sample_role;
+GRANT ALL PRIVILEGES ON SCHEMA markdown_place_schema TO markdown_place_role;
+
+-- テスト用データベース作成
+CREATE DATABASE test_db;
+
+-- データベース切り替え
+\c test_db
+
+-- スキーマ作成
+CREATE SCHEMA markdown_place_schema;
+
+-- ロール作成
+CREATE ROLE markdown_place_role WITH LOGIN PASSWORD 'markdown_place_role';
+
+-- 権限付与
+GRANT ALL PRIVILEGES ON SCHEMA markdown_place_schema TO markdown_place_role;

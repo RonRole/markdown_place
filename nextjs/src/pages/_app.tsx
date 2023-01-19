c1a60158d48cf334bc1a4576019dff0f19fb80f3
RonRole <ronrolenewadress@gmail.com>
2023-01-19 11:03:33 +0900

    cloudbuild テスト

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -3,13 +3,10 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 import dynamic from 'next/dynamic';
 import axios from 'axios';
-import {
-    AlertDialogContextProvider,
-    AuthContextProvider,
-    EditNewArticleDialogContextProvider,
-} from '../components/context';
+import { AuthContextProvider, EditNewArticleDialogContextProvider } from '../components/context';
 import { AxiosInterceptorsSettings } from '../components/functional';
 import { LoadAuthorization } from '../components/functional/LoadAuthorization';
+import { AlertDialogContextProvider } from '../components/context/AlertDialogContextProvider';
 
 /**
  * 内部でwindowを使用しているため、ssrでのエラーを回避するために
